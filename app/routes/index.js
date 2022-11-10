212bfa1ec39722c46122bfc511c3803c111ae160
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-11-10 15:22:30 +0000

    Add headers

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,12 @@ const proxyCall = () => {
       const query = req.url.search ? req.url.search : ''
       const uri = `${remote.url}${req.url.pathname}${query}`
       console.log(`Proxying to ${uri}`)
-      return { uri }
+      const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With'
+      }
+      return { uri, headers }
     },
     passThrough: true,
     xforward: true
